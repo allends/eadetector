a76afc50cc2571d8996b30e4e65431bd9fe16076
allends <allen.davisswing@rutgers.edu>
2023-01-26 08:23:42 -0500

    Initial Commit

---
+++ eADetector/eADetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  eADetectorApp.swift
+//  eADetector
+//
+//  Created by Allen Davis-Swing on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct eADetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
